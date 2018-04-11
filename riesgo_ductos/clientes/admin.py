139ddbe49eb8806e5d8d09d9bd01e1bce7718aab
alejandromoya <alejandro@publiaciertos.net>
2018-04-11 12:08:22 -0400

    Creados y registrados los modelos TipoTel y Telefonos

--- riesgo_ductos/clientes/admin.py
+++ riesgo_ductos/clientes/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import (Telefono, Empresa, Contacto, Proyecto, PnID, Equipo,
+from .models import (Telefono, TipoTel, Empresa, Contacto, Proyecto, PnID, Equipo,
     Historia, HInspeccion, HMantenimiento, HEquipo, Formula, Parametro, 
     HEquipoParametro)
 admin.site.register(Telefono)
+admin.site.register(TipoTel)
 admin.site.register(Empresa)
 admin.site.register(Contacto)
 admin.site.register(Proyecto)
--- riesgo_ductos/clientes/models.py
+++ riesgo_ductos/clientes/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+class TipoTel(models.Model):
+    nombre = models.CharField(max_length=255)
+    def __str__(self):
+        return self.nombre
+
 class Telefono(models.Model):
     telefono = models.IntegerField(primary_key=True)
-
-    def __str__(self):
+    tipo = models.ForeignKey(TipoTel, on_delete=models.PROTECT)
+    def __int__(self):
         return self.telefono
-
 
 class Empresa(models.Model):
     código = models.CharField(primary_key=True, max_length=255)
