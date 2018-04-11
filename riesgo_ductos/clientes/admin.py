c938686b1db692bc9813e773345c654a3cec1b46
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2018-04-10 22:11:10 -0400

    añadi los modelos al admin

--- riesgo_ductos/clientes/admin.py
+++ riesgo_ductos/clientes/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 
-from .models import Equipo, Empresa, Telefono, Contacto
-admin.site.register(Equipo)
-admin.site.register(Empresa)
+from .models import (Telefono, Empresa, Contacto, Proyecto, PnID, Equipo,
+    Historia, HInspeccion, HMantenimiento, HEquipo, Formula, Parametro, 
+    HEquipoParametro)
 admin.site.register(Telefono)
+admin.site.register(Empresa)
 admin.site.register(Contacto)
+admin.site.register(Proyecto)
+admin.site.register(PnID)
+admin.site.register(Equipo)
+admin.site.register(Historia)
+admin.site.register(HInspeccion)
+admin.site.register(HMantenimiento)
+admin.site.register(HEquipo)
+admin.site.register(Formula)
+admin.site.register(Parametro)
+admin.site.register(HEquipoParametro)
