93149a08ecbb94eb489e2f96df0027186f0ea01f
Teo Panda <antonio@publiaciertos.net>
2018-04-13 00:11:02 -0400

    Test crear Equipo (con todas sus dependencias) y crear Parametro

--- riesgo_ductos/clientes/tests.py
+++ riesgo_ductos/clientes/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import Equipo, Empresa, Proyecto, PnID, Parametro
 
-# Create your tests here.
+def crear_equipo(nombre):
+    """
+    Crea un equipo dado un nombre
+    """
+    empresa = Empresa.objects.create(código=5, nombre="Empresa 1")
+    proyecto = Proyecto.objects.create(nombre="Proyecto 1", empresa=empresa)
+    pnid = PnID.objects.create(nombre="PnID 1", proyecto=proyecto)
+    equipo = Equipo.objects.create(nombre="E1", cliente=empresa, pnid=pnid)
+    return equipo
+
+class EquipoTest(TestCase):
+    def test_crea_equipo(self):
+        crear_equipo(nombre="E1")
+        self.assertQuerysetEqual(
+            Equipo.objects.all(),
+            ['<Equipo: E1>']
+        )
+
+class ParametroTest(TestCase):
+    def test_crea_parametro(self):
+        parametro = Parametro(nombre="Parametro 1")
+        self.assertQuerysetEqual(
+            Parametro.objects.all(),
+            []
+        )
+        parametro.save()
+        self.assertQuerysetEqual(
+            Parametro.objects.all(),
+            ['<Parametro: Parametro 1>']
+        )
